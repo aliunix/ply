4f47b74c8de2121d315689ab77d5c481a1b81b25
Papa Alioune Ly <aliunix@me.com>
2012-11-06 19:14:34 +0100

    haha... and another commit for fun!

--- Sources/LastGitTest/src/test/SecondFile.java
+++ Sources/LastGitTest/src/test/SecondFile.java
@@ -7,6 +7,8 @@ public class SecondFile {
 	 */
 	public static void main(String[] args) {
 		System.out.println("I'm a second file..boom!");
+		
+		System.out.println("haha... and another commit for fun!");
 
 	}
 
